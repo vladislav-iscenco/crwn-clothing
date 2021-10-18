8f973e80fda8f8d0da7602a544a71cceec294445
viscenco <vladislav.iscenco@gmail.com>
2021-10-18 20:47:15 +0300

    addin header component and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx'
 
 
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
