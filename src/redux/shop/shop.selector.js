73b8a1797b45e1c18c25d7bdafbcddfa5df11425
viscenco <vladislav.iscenco@gmail.com>
2021-10-29 19:26:11 +0300

    container pattern and redux thunk

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -18,9 +18,19 @@ export const selectCollectionsForPreview = createSelector(
 
 export const selectCollection = memoize(collectionUrlParam =>
     createSelector(
-        [selectCollections],
+        [selectCollections], 
         collections => collections
             ? collections[collectionUrlParam]
             : null
     )
 );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+)
